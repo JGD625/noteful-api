6dc0d99ce8a62411710a2145b9146d0c4c355d17
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-16 09:13:51 -0400

    Update folders-service.js

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -1,37 +1,37 @@
-const FoldersService = {
-  getAllFolders(knex){
-    return knex.select('*').from('noteful_folders');
-  },
-
-  insertFolder(knex, newFolder){
-    return knex
-      .insert(newFolder)
-      .into('noteful_folders')
-      .returning('*')
-      .then(rows => {
-        return rows[0];
-      });
-  },
-
-  getFolderById(knex, id) {
-    return knex
-      .from('noteful_folders')
-      .select('*')
-      .where('id', id)
-      .first();
-  },
-
-  deleteFolder(knex, id){
-    return knex('noteful_folders')
-      .where({ id })
-      .delete(); 
-  },
-
-  updateFolder(knex, id, newFolderFields){
-    return knex('noteful_folders')
-      .where({ id })
-      .update(newFolderFields);
-  },
-};
-
-module.exports = FoldersService; 
+const foldersService = {
+    //getall
+    getAllFolders(knex) {
+        return knex.select('*').from('folders')
+    },
+    //insert
+    insertFolder(knex, newFolder) {
+        return knex
+          .insert(newFolder)
+          .into('folders')
+          .returning('*')
+          .then(rows => {
+              return rows[0]
+          })
+    },
+    //getById
+    getById(knex, id) {
+        return knex 
+          .from('folders')
+          .select('*')
+          .where('id', id)
+          .first()
+    },
+    //delete
+    deleteFolder(knex, id) {
+        return knex('folders')
+          .where({ id })
+          .delete()
+    },
+    //update
+    updateFolder(knex, id, newFolderFields) {
+        return knex('folders')
+          .where({ id })
+          .update(newFolderFields)
+    }
+}
+module.exports = foldersService
