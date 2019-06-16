6c2044686d955a85b92d104f6a748df8ba5059b3
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-16 09:00:34 -0400

    Update note.fixtures.js

--- test/note.fixtures.js
+++ test/note.fixtures.js
@@ -1,61 +1,22 @@
-function makeTestNotes() {
-  return [
-      {notes_name: 'How to Dance With Dogs',
-      id: 1,
-      content: 'Watch out for their paws.',
-      modified: "2019-03-17T21:22:26.221Z",
-      folder_id: 1
-      },
-      {notes_name: 'How to Dance With Cats',
-      id: 2,
-      content: 'They are wallflowers.',
-      modified: "2019-03-17T21:22:26.221Z",
-      folder_id: 2
-      },
-      {notes_name: 'Up is down',
-      id: 3,
-      content: 'Down is up',
-      modified: "2019-03-17T21:22:26.221Z",
-      folder_id: 3
-      },
-      {notes_name: 'Left is right',
-      id: 4,
-      content: 'Right is left',
-      modified: "2019-03-17T21:22:26.221Z",
-      folder_id: 1
-      },
-      {notes_name: 'Who are you?',
-      id: 5,
-      content: 'Who who?',
-      modified: "2019-03-17T21:22:26.221Z",
-      folder_id: 3
-      },
-  ]
+function makeNotesArray() {
+    return [
+        {
+            id: 1,
+            name: 'First Note',
+            folder_id: 1,
+            content: 'This is the first note, tis a good note.',
+            modified_date: '2020-01-22'
+        },
+        {
+            id: 2,
+            name: 'Second Note',
+            folder_id: 2,
+            content: 'This is the second note, also a grand ole note.',
+            modified_date: '2020-01-22'
+        }
+    ]
 }
 
-function newNoteTest() {
-  return {
-  notes_name: 'My dog is Po',
-  content: 'Because she is a po, po little dog.',
-  folder_id: 3
-  }
+module.exports = {
+    makeNotesArray
 }
-
-function updateNote() {
-  return {
-      notes_name: 'Update name',
-      content: 'Updated content',
-  }
-}
-
-function expectedUpdateNote() {
-  return {
-      notes_name: 'Update name',
-      content: 'Updated content',
-      modified: "2019-03-17T21:22:26.221Z",
-      folder_id: 2,
-      id: 2
-  }
-}
-
-module.exports = {makeTestNotes, newNoteTest, updateNote, expectedUpdateNote}
