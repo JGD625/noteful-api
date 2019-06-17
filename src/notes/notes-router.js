b9931773c1f695d69d15d8a2dd064c0c6e01b00e
Julie <39500350+JGD625@users.noreply.github.com>
2019-06-17 17:57:13 -0400

    Update notes-router.js

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -6,97 +6,83 @@ const NotesService = require('./notes-service');
 const notesRouter = express.Router();
 const jsonParser = express.json();
 
-const serializedNote = note => ({
-    id: note.id,
-    note_name: note.note_name,
-    folder_id: note.folder_id,
-    content: note.content,
-    modified_date: note.modified_date
-})
+function sanitize(note) {
+  return {
+    id : note.id,
+    name : xss(note.name),
+    modified : xss(note.modified),
+    folderId : note.folderid,
+    content : xss(note.content)
+  };
+}
 
-notesRouter
-    .route('/')
-    .get((req, res, next) => {
-        const knexInstance = req.app.get('db')
-        NotesService.getAllNotes(knexInstance)
-        .then(notes => {
-            res.json(notes.map(serializedNote))
-        })
-        .catch(next)
-    })
-    .post(jsonParser, (req, res, next) => {
-        const { note_name, content,folder_id, modified_date } = req.body
-        const newNote = { note_name, content,folder_id, modified_date }
+notesRouter.route('/noteful')
+  .get((req, res) => {
+    const db = req.app.get('db');
 
-        for ( const [key, value] of Object.entries(newNote))
-            if (value == null)
-            return res.status(400).json({
-                error: { message: `Missing ${key} in request body`}
-            })
-        NotesService.insertNote(
-            req.app.get('db'),
-            newNote
-        )
-        .then(note => {
-            res
-                .status(201)
-                .location(path.posix.join(req.originalUrl, `/${note.id}`))
-                .json(serializedNote(note))
-        })
-        .catch(next)    
-    })
-notesRouter
-    .route('/:id')
-    .all((req, res, next) => {
-        NotesService.getById(
-            req.app.get('db'),
-            req.params.id
-        )
-        .then(note => {
-            if(!note) {
-                return res.status(404).json({
-                    error: { message: `Note doesn't exist`}
-                })
-            }
-            res.note = note
-            next()
-        })
-        .catch(next)
-    })
-    .get((req, res, next) => {
-        res.json(serializedNote(res.note))
-    })
-    .delete((req, res, next) => {
-        NotesService.deleteNote(
-            req.app.get('db'),
-            req.params.id
-        )
-        .then(numRowsAffected => {
-            let noteId = req.params.id
-            res.status(200).json({noteId})
-        })
-        .catch(next)
-    })
-    .patch(jsonParser, (req, res, next) => {
-        const { note_name, folder_id, content, modified_date } = req.body
-        const noteToUpdate = {note_name,folder_id, content, modified_date }
-        const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length
-        if(numberOfValues === 0) {
-            return res.status(400).json({
-                error: {
-                    message: `Request body must contain either 'name' or 'content'`
-                }
-            })
+    return notes
+      .getAllNotes(db)
+      .then((data => {
+        res.json(data.map(sanitize));
+      }));
+  })
+  .post((req, res) => {
+    const { id, name, modified, folderid, content } = req.body;
+
+    const note = {
+      id,
+      name,
+      modified,
+      folderid,
+      content
+    };
+    const db = req.app.get('db');
+
+    notes.insertNote(db, note).then(resjson => {
+      res.status(201).json(resjson);
+    });
+  });
+
+notesRouter.route('/note/:id')
+  .get((req, res) => {
+    const db = req.app.get('db');
+
+    return notes
+      .getById(db,req.params.id)
+      .then(resjson=> {
+        if (resjson.length > 0) {
+          res.json(resjson.map(sanitize));
         }
-        NotesService.updateNote(
-            req.app.get('db'),
-            req.params.id,
-            noteToUpdate
-        )
-        .then(numRowsAffected => {
-            res.status(204).end()
-        })
-        .catch(next)
-    })
+        else{
+          res.status(404).end();
+        }
+      });
+  })
+  .delete((req, res) => {
+    const db = req.app.get('db');
 
-    module.exports = notesRouter
+    return notes
+      .deleteNote(db, req.params.id)
+      .then(resjson => {
+        if (resjson === 1) {
+          res.status(204).end();
+        } else {
+          res.status(404).end();
+        }
+      });
+  })
+  .patch((req, res) =>{
+    const db = req.app.get('db');
+
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).end();
+    }
+
+    return notes
+      .updateNote(db,req.params.id, req.body)
+      .then(resjson => {
+        res.status(201).json(resjson);
+      });
+  });
+
+module.exports = notesRouter;
